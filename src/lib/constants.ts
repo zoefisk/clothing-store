e3e6058d38d3a6299b5fbe18d1762638e6fc3a3c
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-04-25 18:22:32 -0400

    Created constants for clothing types, styles, sizes, and colors

---
+++ src/lib/constants.ts
@@ -0,0 +1,35 @@
+const clothingTypes = [
+    'Tops',
+    'Bottoms',
+    'Dresses',
+    'Outerwear',
+    'Footwear',
+    'Accessories',
+];
+
+const clothingSyles = [
+    'Casual',
+    'Formal',
+    'Sporty',
+    'Bohemian',
+    'Streetwear',
+    'Vintage',
+];
+
+const clothingSizes = [
+    'XS',
+    'S',
+    'M',
+    'L',
+    'XL',
+    'XXL',
+];
+
+const clothingColors = [
+    'Red',
+    'Blue',
+    'Green',
+    'Black',
+    'White',
+    'Yellow',
+];
