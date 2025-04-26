a7e4ce1a1c3899e9cd01972a185b8d297b08d471
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-04-26 15:37:53 -0400

    added breadcrumbs to slug page

--- src/app/products/[productSlug]/page.tsx
+++ src/app/products/[productSlug]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import {notFound, useParams} from "next/navigation";
+import { notFound, useParams } from "next/navigation";
 import { Product } from "@/models/Product";
-import {fetchImagesByProductId, fetchProductBySlug, fetchProducts} from "@/utils/api";
-import { Skeleton, Text, Image, Badge, Button, Container } from "@mantine/core";
+import { fetchImagesByProductId, fetchProductBySlug } from "@/utils/api";
+import { Skeleton, Text, Image, Badge, Button, Container, Breadcrumbs, Anchor } from "@mantine/core";
 import P from "@/components/typography/P";
-import {ProductImage} from "@/models/ProductImage";
+import { ProductImage } from "@/models/ProductImage";
 
 export default function ProductDetailsPage() {
     const { productSlug } = useParams() as { productSlug: string };
@@ -31,6 +31,7 @@ export default function ProductDetailsPage() {
 
         fetchProduct();
     }, [productSlug]);
+
     useEffect(() => {
         const fetchImages = async () => {
             if (product) {
@@ -58,10 +59,21 @@ export default function ProductDetailsPage() {
         notFound();
     }
 
-    console.log("Product images: ", images);
+    const breadcrumbItems = [
+        { title: "Home", href: "/" },
+        { title: "Products", href: "/products" },
+        { title: product.name, href: `/products/${product.slug}` },
+    ].map((item, index) => (
+        <Anchor href={item.href} key={index}>
+            {item.title}
+        </Anchor>
+    ));
 
     return (
         <Container>
+            <Breadcrumbs separator="→" mt="md" mb="lg">
+                {breadcrumbItems}
+            </Breadcrumbs>
             <Image src={images[0]?.url} alt={product.name} height={300} fit="cover" />
             <Text fw={700} size="xl" mt="md">{product.name}</Text>
             <Badge color="pink" size="lg" mt="sm">${product.price}</Badge>
