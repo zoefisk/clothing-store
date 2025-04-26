abec90fd6a9681c2f323697635ea1770fdcea7d8
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-04-26 15:15:06 -0400

    updated P styling

--- src/components/typography/P.tsx
+++ src/components/typography/P.tsx
@@ -1,6 +1,6 @@
 export default function P({ children }: { children: React.ReactNode }) {
     return (
-        <p className="text-xs font-bold text-gray-900 dark:text-white">
+        <p className="text-s text-gray-900 dark:text-white">
             {children}
         </p>
     );
