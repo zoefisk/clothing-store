e164f14318b26824000d9b92322a7cd63501b7f7
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-04-26 21:42:24 -0400

    added pagination

--- src/components/products/ProductGrid.tsx
+++ src/components/products/ProductGrid.tsx
@@ -1,21 +1,42 @@
-import {Product} from "@/models/Product";
+import { useState } from "react";
+import { Product } from "@/models/Product";
 import P from "@/components/typography/P";
-import {Grid} from "@mantine/core";
+import { Grid, Pagination } from "@mantine/core";
 import ProductCard from "@/components/products/ProductCard";
 
+function chunk<T>(array: T[], size: number): T[][] {
+  if (!array.length) {
+    return [];
+  }
+  const head = array.slice(0, size);
+  const tail = array.slice(size);
+  return [head, ...chunk(tail, size)];
+}
+
 export default function ProductGrid({ products }: { products: Product[] }) {
+  const [activePage, setPage] = useState(1);
+  const productsPerPage = 20;
+  const paginatedProducts = chunk(products, productsPerPage);
 
-    if (products.length === 0) {
-        return <P>No products match the given search query.</P>;
-    }
+  if (products.length === 0) {
+    return <P>No products match the given search query.</P>;
+  }
 
-    return (
-        <Grid>
-            {products.map((product) => (
-                <Grid.Col key={product.id} span={2.7}>
-                    <ProductCard product={product}/>
-                </Grid.Col>
-            ))}
-        </Grid>
-    )
+  return (
+    <>
+      <Grid>
+        {paginatedProducts[activePage - 1].map((product) => (
+          <Grid.Col key={product.id} span={2.7}>
+            <ProductCard product={product} />
+          </Grid.Col>
+        ))}
+      </Grid>
+      <Pagination
+        total={paginatedProducts.length}
+        value={activePage}
+        onChange={setPage}
+        mt="lg"
+      />
+    </>
+  );
 }
