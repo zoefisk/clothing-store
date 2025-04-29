e2d8241703e31ef544ff65f59a1e02f344c184ce
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-04-29 18:30:56 -0400

    added category icon to categories menu button

--- src/components/navigation/CategoriesMenu.tsx
+++ src/components/navigation/CategoriesMenu.tsx
@@ -5,45 +5,49 @@ import '@mantine/core/styles.css';
 import NavButton from "@/components/navigation/NavButton";
 import H3 from "@/components/typography/H3";
 import {clothingTypes, clothingColors, clothingSizesShorthand, clothingSizes, clothingStyles} from "@/lib/constants";
+import {IconCategory} from "@tabler/icons-react";
 
 export default function CategoriesMenu() {
 
     return (
-        <Menu trigger="hover" openDelay={100} closeDelay={400}>
-            <Menu.Target>
-                <NavButton>
-                    Categories
-                </NavButton>
-            </Menu.Target>
+        <>
+            <Menu trigger="hover" openDelay={100} closeDelay={400}>
+                <Menu.Target>
+                    <NavButton>
+                        Categories
+                        <IconCategory size={18} style={{ marginRight: 8, marginLeft: 5 }} />
+                    </NavButton>
+                </Menu.Target>
 
-            <Menu.Dropdown>
-                {/*<Menu.Item>Option 1</Menu.Item>*/}
-                {/*<Menu.Item>Option 2</Menu.Item>*/}
-                {/*<Menu.Item>Option 3</Menu.Item>*/}
+                <Menu.Dropdown>
+                    {/*<Menu.Item>Option 1</Menu.Item>*/}
+                    {/*<Menu.Item>Option 2</Menu.Item>*/}
+                    {/*<Menu.Item>Option 3</Menu.Item>*/}
 
-                <Grid gutter={"xl"}>
-                    <Grid.Col span={4}>
-                        <H3>Types</H3>
-                            {clothingTypes.map((type) => (
-                                <p key={type}>{type}</p>
+                    <Grid gutter={"xl"}>
+                        <Grid.Col span={4}>
+                            <H3>Types</H3>
+                                {clothingTypes.map((type) => (
+                                    <p key={type}>{type}</p>
+                                ))}
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <H3>Styles</H3>
+                            {clothingStyles.map((style) => (
+                                <p key={style}>{style}</p>
                             ))}
-                    </Grid.Col>
-                    <Grid.Col span={4}>
-                        <H3>Styles</H3>
-                        {clothingStyles.map((style) => (
-                            <p key={style}>{style}</p>
-                        ))}
-                    </Grid.Col>
-                    <Grid.Col span={4}>
-                        <H3>Sizes</H3>
-                        {clothingSizes.map((size) => (
-                            <p key={size}>{size}</p>
-                        ))}
-                    </Grid.Col>
-                </Grid>
+                        </Grid.Col>
+                        <Grid.Col span={4}>
+                            <H3>Sizes</H3>
+                            {clothingSizes.map((size) => (
+                                <p key={size}>{size}</p>
+                            ))}
+                        </Grid.Col>
+                    </Grid>
 
 
-            </Menu.Dropdown>
-        </Menu>
+                </Menu.Dropdown>
+            </Menu>
+        </>
     );
 }
