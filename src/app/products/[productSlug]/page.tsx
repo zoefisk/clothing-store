5c02453a3a8b096ef83600fb375b81eb6c510ce9
zoefisk <94054962+zoefisk@users.noreply.github.com>
2025-04-29 18:24:46 -0400

    simplified code of product slug page. improved styling.

--- src/app/products/[productSlug]/page.tsx
+++ src/app/products/[productSlug]/page.tsx
@@ -1,27 +1,24 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import {notFound, useParams, useSearchParams} from "next/navigation";
+import { notFound, useParams, useSearchParams } from "next/navigation";
 import { Product } from "@/models/Product";
 import {
-    fetchImagesByProductId,
     fetchProductBySlug,
     fetchProducts,
     fetchReviewsByProductId,
-    fetchStockLevelsByProductId
+    fetchStockLevelsByProductId,
+    fetchImageByImageId
 } from "@/utils/api";
-import { Skeleton, Text, Image, Badge, Button, Container, Breadcrumbs, Anchor, Grid, Textarea, Title } from "@mantine/core";
+import { Skeleton, Image, Container, Breadcrumbs, Anchor, Grid, Title } from "@mantine/core";
 import P from "@/components/typography/P";
-import { ProductImage } from "@/models/ProductImage";
 import ProductDetails from "@/components/products/ProductDetails";
-import {getSignedInUser} from "@/utils/auth";
 import ReviewList from "@/components/reviews/ReviewList";
-import {Review} from "@/models/Review";
-
 import ViewMoreProductsCarousel from "@/components/products/ViewMoreProductsCarousel";
-import SizeSelector from "@/components/products/SizeSelector";
-import ColorSelector from "@/components/products/ColorSelector";
-import {StockLevel} from "@/models/StockLevel";
+import { StockLevel } from "@/models/StockLevel";
+import { Review } from "@/models/Review";
+import {ProductImage} from "@/models/ProductImage";
+import {convertURLSize} from "@/utils/conversions";
 
 export default function ProductDetailsPage() {
     const { productSlug } = useParams() as { productSlug: string };
@@ -30,10 +27,10 @@ export default function ProductDetailsPage() {
 
     const [product, setProduct] = useState<Product | null>(null);
     const [stockLevels, setStockLevels] = useState<StockLevel[]>([]);
-    const [currentQuantity, setCurrentQuantity] = useState<number | null>(null);
+    const [currentStockLevel, setCurrentStockLevel] = useState<StockLevel | null>(null);
+    const [currentImage, setCurrentImage] = useState<ProductImage | null>(null);
     const [similarProducts, setSimilarProducts] = useState<Product[]>([]);
     const [reviews, setReviews] = useState<Review[]>([]);
-    const [images, setImages] = useState<ProductImage[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
@@ -73,50 +70,47 @@ export default function ProductDetailsPage() {
     }, [product]);
 
     useEffect(() => {
-        const urlColor = searchParams.get("color");
-        let urlSize = searchParams.get("size");
-        switch (urlSize) {
-            case "extra-small":
-                urlSize = "XS";
-                break;
-            case "small":
-                urlSize = "S";
-                break;
-            case "medium":
-                urlSize = "M";
-                break;
-            case "large":
-                urlSize = "L";
-                break;
-            case "extra-large":
-                urlSize = "XL";
-                break;
-            case "double-extra-large":
-                urlSize = "XXL";
-                break;
-            default:
-                urlSize = null;
-        }
+        const fetchCurrentStockLevel = async () => {
+            const urlColor = searchParams.get("color");
+            let urlSize = searchParams.get("size");
+            urlSize = convertURLSize(urlSize);
 
-        console.log("stock levels: ", stockLevels);
+            if (urlColor && urlSize && stockLevels.length > 0) {
+                const matchingStock = stockLevels.find(
+                    (stock) => stock.color === urlColor && stock.size === urlSize
+                );
 
-        if (urlColor && urlSize && stockLevels.length > 0) {
-            const matchingStock = stockLevels.find(
-                (stock) => stock.color === urlColor && stock.size === urlSize
-            );
-            setCurrentQuantity(matchingStock ? matchingStock.quantity : null);
-            console.log("Matching stock:", matchingStock);
-            console.log("Current quantity:", currentQuantity);
-        } else {
-            setCurrentQuantity(null);
-        }
+                if (matchingStock) {
+                    setCurrentStockLevel(matchingStock);
+
+                    if (matchingStock.imageId) {
+                        try {
+                            const image = await fetchImageByImageId(matchingStock.imageId);
+                            setCurrentImage(image);
+                        } catch (error) {
+                            console.error("Error fetching image:", error);
+                        }
+                    } else {
+                        setCurrentImage(null);
+                    }
+                } else {
+                    setCurrentStockLevel(null);
+                    setCurrentImage(null);
+                }
+            } else {
+                setCurrentStockLevel(null);
+                setCurrentImage(null);
+            }
+        };
+
+        fetchCurrentStockLevel();
     }, [searchParams, stockLevels]);
 
     useEffect(() => {
         const fetchSimilarProducts = async () => {
             try {
                 const products = await fetchProducts();
-                const filteredProducts = products.filter((p) => p.slug !== productSlug);    // Exclude the current product
+                const filteredProducts = products.filter((p) => p.slug !== productSlug);
                 setSimilarProducts(filteredProducts);
             } catch (error) {
                 console.error("Error fetching similar products:", error);
@@ -125,25 +119,6 @@ export default function ProductDetailsPage() {
 
         fetchSimilarProducts();
     }, []);
-
-    useEffect(() => {
-        const fetchImages = async () => {
-            if (product) {
-                try {
-                    if (product.id !== undefined) {
-                        const images = await fetchImagesByProductId(product.id);
-                        setImages(images);
-                    } else {
-                        console.error("Product ID is undefined");
-                    }
-                } catch (error) {
-                    console.error("Error fetching product images:", error);
-                }
-            }
-        };
-
-        fetchImages();
-    }, [product]);
 
     useEffect(() => {
         const fetchReviews = async () => {
@@ -178,32 +153,56 @@ export default function ProductDetailsPage() {
     ));
 
     return (
-        <Container>
+        <Container style={{ padding: "0", maxWidth: "80%" }}>
             <Breadcrumbs separator="→" mt="md" mb="lg">
                 {breadcrumbItems}
             </Breadcrumbs>
 
-            <Grid>
-                {/* Left Column: Image */}
-                <Grid.Col span={6}>
-                    <Image src={images[0]?.url} alt={product.name} height={300} fit="cover" />
+            <Grid gutter="50">
+                {/* Main Content */}
+                <Grid.Col span={8}>
+                    <Grid gutter="sm">
+                        {/* Left Column: Image */}
+                        <Grid.Col span={6}>
+                            {currentImage ? (
+                                <Image
+                                    src={currentImage.url}
+                                    alt={currentImage.altText}
+                                    height={300}
+                                    width={300}
+                                    fit="cover"
+                                    // style={{ border: "1.5px solid grey" }}
+                                />
+                            ) : (
+                                <Skeleton visible={true} height={300} />
+                            )}
+                        </Grid.Col>
+                        <ProductDetails product={product} />
+                    </Grid>
+
+                    <P>
+                        {currentStockLevel?.quantity === 0
+                            ? "Sorry! This item is currently out of stock with this size and color combination."
+                            : (currentStockLevel?.quantity ?? 0) <= 5
+                                ? "Low stock! Grab yours before it's gone!"
+                                : ``}
+                    </P>
+
+                    {/* Reviews and Comments Section */}
+                    <ReviewList reviews={reviews} />
                 </Grid.Col>
-                <ProductDetails product={product} />
+
+                {/* Right Column: Vertical Carousel */}
+                <Grid.Col span={4} style={{ textAlign: "center" }}>
+                    <Title order={3} mt="xl" mb="md">View More Like This</Title>
+                    <hr/>
+                    {similarProducts.length > 0 ? (
+                        <ViewMoreProductsCarousel products={similarProducts} orientation={"vertical"} />
+                    ) : (
+                        <P>No similar items found.</P>
+                    )}
+                </Grid.Col>
             </Grid>
-
-            <P>Current quantity: {currentQuantity}</P>
-
-            {/* Reviews and Comments Section */}
-            <hr/>
-            <ReviewList reviews={reviews}/>
-
-            {/* Similar Items Section */}
-            <Title order={3} mt="xl" mb="md">View More Like This</Title>
-            {similarProducts.length > 0 ? (
-                <ViewMoreProductsCarousel products={similarProducts} />
-            ) : (
-                <P>No similar items found.</P>
-            )}
         </Container>
     );
 }
